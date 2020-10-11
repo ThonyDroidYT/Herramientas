bb32d5bc9c0823159ab5ccf7ee87b35940304bfc
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2020-10-11 12:21:48 -0500

    Update wg.sh

--- wg.sh
+++ wg.sh
@@ -18,6 +18,12 @@ cyan='\033[1;36m'
 #Barra
 BARRA='${blue}======================================================'
 
+# Detect Debian users running the script with "sh" instead of bash
+if readlink /proc/$$/exe | grep -q "dash"; then
+	echo 'This installer needs to be run with "bash", not "sh".'
+	exit
+fi
+
 # Discard stdin. Needed when running from an one-liner which includes a newline
 read -N 999999 -t 0.001
 
