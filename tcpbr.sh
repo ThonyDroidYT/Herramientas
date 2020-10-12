539953ebcada63187538b0a4369cc85832f06302
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2020-10-12 11:33:44 -0500

    Update tcpbr.sh

--- tcpbr.sh
+++ tcpbr.sh
@@ -373,9 +373,9 @@ Update_Shell(){
 # Menú de inicio
 start_menu(){
 clear
-msg -bar
+echo -e "\033[1;34m======================================================"
 echo -e " TCP Aceleración (BBR/Plus) MOD By @Thony_DroidYT ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}
-msg -bar
+echo -e "\033[1;34m======================================================"
  ${Green_font_prefix}0.${Font_color_suffix} Script de actualización
 ————————————Gestión del Núcleo————————————
  ${Green_font_prefix}1.${Font_color_suffix} Instalación BBR/BBR Kernel Mágico
