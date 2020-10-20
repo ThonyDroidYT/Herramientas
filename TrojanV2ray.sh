212505116a9974d8bf4c2001103cc4e21bc3e28b
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2020-10-20 18:57:53 -0500

    Update TrojanV2ray.sh

--- TrojanV2ray.sh
+++ TrojanV2ray.sh
@@ -2085,13 +2085,13 @@ function start_menu(){
         getLinuxOSVersion
         ${osSystemPackage} -y install wget curl git
     fi
-
+    clear
     green " =================================================="
     green " Trojan Trojan-go V2ray 一键安装脚本 2020-9-10 更新.  系统支持：centos7+ / debian9+ / ubuntu16.04+"
     red " *请不要在任何生产环境使用此脚本 请不要有其他程序占用80和443端口"
     red " *若是已安装trojan 或第二次使用脚本，请先执行卸载trojan"
     green " =================================================="
-    green " 1. 安装 BBR-PLUS 加速4合一脚本"
+    green " 1. Instalar BBR-PLUS 4 en 1 para acelerar el Script"
     echo
     green " 2. 安装 trojan 和 nginx 不支持CDN"
     green " 3. 修复证书 并继续安装 trojan"
