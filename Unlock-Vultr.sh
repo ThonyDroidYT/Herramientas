c34c579e68a84575a028935bf33653e5d172220c
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2020-11-01 20:38:15 -0500

    Update Unlock-Vultr.sh

--- Unlock-Vultr.sh
+++ Unlock-Vultr.sh
@@ -22,18 +22,18 @@ echo -e " \033[1;33m[\033[1;34m####################\033[1;33m] - \033[1;32m100%\
 sleep 1s
 }
 pamcrack () {
-echo -e "${cor[5]}LIBERANDO PASSWD PARA VURTL"
+echo -e "${cor[5]}LIBERANDO PASSWD PARA VULTR"
 sed -i 's/.*pam_cracklib.so.*/password sufficient pam_unix.so sha512 shadow nullok try_first_pass #use_authtok/' /etc/pam.d/common-password
 echo -e "${cor[5]}LISTO YA PUEDES CREAR USUARIOS"
 }
 
 shadow_fun () {
-echo -e " ${cor[8]}PERMISO ROOT VURTL ${cor[3]}[NEW-ADM-PLUS]\033[0m"
+echo -e " ${cor[8]}PERMISO ROOT VULTR ${cor[3]}[NEW-ADM-PLUS]\033[0m"
 echo -e "$barra"
 while true; do
-echo -e "${cor[4]} [1] > ${cor[8]}DESBLOQUEAR VURTL PARA CREAR USUARIOS"
-echo -e "${cor[4]} [2] > ${cor[0]}VOLVER"
-echo -e "${cor[4]} [0] > ${cor[0]}SALIR\n${barra}"
+echo -e "${cor[3]} [1] ${cor[3]}> ${cor[8]}DESBLOQUEAR VULTR PARA CREAR USUARIOS"
+echo -e "${cor[3]} [2] ${cor[3]}> ${cor[0]}VOLVER"
+echo -e "${cor[3]} [0] ${cor[3]}> ${cor[0]}SALIR\n${barra}"
 while [[ ${opx} != @(0|[1-2]) ]]; do
 echo -ne "${cor[0]}Digite una Opcion: \033[1;37m" && read opx
 tput cuu1 && tput dl1
