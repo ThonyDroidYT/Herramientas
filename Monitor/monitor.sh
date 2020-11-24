2fa481106e97c9dcb7c2b0807b72f702a351a3a1
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2020-11-24 14:09:48 -0500

    Update monitor.sh

--- Monitor/monitor.sh
+++ Monitor/monitor.sh
@@ -120,7 +120,7 @@ PT=$(lsof -V -i tcp -P -n | grep -v "ESTABLISHED" |grep -v "COMMAND" | grep "LIS
 for porta in `echo -e "$PT" | cut -d: -f2 | cut -d' ' -f1 | uniq`; do
 svcs=$(echo -e "$PT" | grep -w "$porta" | awk '{print $1}' | uniq)
 #echo -e "\033[1;33m ➾ \e[1;31m $svcs :\033[1;33m ➢ \e[1;32m $porta   "
-<font color="red"> $svcs </font><font color="yellow"> ➢ </font><font color="green"> $porta </font>
+<font color="yellow"> ➾ </font><font color="red"> $svcs : </font><font color="yellow"> ➢ </font><font color="green"> $porta </font>
 done
 }
 
