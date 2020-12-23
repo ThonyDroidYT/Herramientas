e300fef89162a96f8ccee9afa6765d786f9422b7
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2020-12-23 13:05:54 -0500

    Update puertos.sh

--- puertos.sh
+++ puertos.sh
@@ -5,40 +5,39 @@
 #	CONTATO TELEGRAM:	http://t.me/crazy_vpn
 #	CANAL TELEGRAM:	http://t.me/sshplus
 #====================================================
-#[[ $(awk -F" " '{print $2}' /usr/lib/licence) == "@CRAZY_VPN" ]]
-{
-	ram1=$(free -h | grep -i mem | awk {'print $2'})
-	ram2=$(free -h | grep -i mem | awk {'print $4'})
-	ram3=$(free -h | grep -i mem | awk {'print $3'})
-	uso=$(top -bn1 | awk '/Cpu/ { cpu = "" 100 - $8 "%" }; END { print cpu }')
-	system=$(cat /etc/issue.net)
-	fun_bar() {
-		comando[0]="$1"
-		comando[1]="$2"
-		(
-			[[ -e $HOME/fim ]] && rm $HOME/fim
-			[[ ! -d /etc/SSHPlus ]] && rm -rf /bin/menu
-			${comando[0]} >/dev/null 2>&1
-			${comando[1]} >/dev/null 2>&1
-			touch $HOME/fim
-		) >/dev/null 2>&1 &
-		tput civis
-		echo -ne "\033[1;33mAGUARDE \033[1;37m- \033[1;33m["
-		while true; do
-			for ((i = 0; i < 18; i++)); do
-				echo -ne "\033[1;31m#"
-				sleep 0.1s
-			done
-			[[ -e $HOME/fim ]] && rm $HOME/fim && break
-			echo -e "\033[1;33m]"
-			sleep 1s
-			tput cuu1
-			tput dl1
-			echo -ne "\033[1;33mAGUARDE \033[1;37m- \033[1;33m["
-		done
-		echo -e "\033[1;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
-		tput cnorm
-	}
+#[[ $(awk -F" " '{print $2}' /usr/lib/licence) == "@CRAZY_VPN" ]] &&{
+#	ram1=$(free -h | grep -i mem | awk {'print $2'})
+#	ram2=$(free -h | grep -i mem | awk {'print $4'})
+#	ram3=$(free -h | grep -i mem | awk {'print $3'})
+#	uso=$(top -bn1 | awk '/Cpu/ { cpu = "" 100 - $8 "%" }; END { print cpu }')
+#	system=$(cat /etc/issue.net)
+#	fun_bar() {
+#		comando[0]="$1"
+#		comando[1]="$2"
+#		(
+#			[[ -e $HOME/fim ]] && rm $HOME/fim
+#			[[ ! -d /etc/SSHPlus ]] && rm -rf /bin/menu
+#			${comando[0]} >/dev/null 2>&1
+#			${comando[1]} >/dev/null 2>&1
+#			touch $HOME/fim
+#		) >/dev/null 2>&1 &
+#		tput civis
+#		echo -ne "\033[1;33mAGUARDE \033[1;37m- \033[1;33m["
+#		while true; do
+#			for ((i = 0; i < 18; i++)); do
+#				echo -ne "\033[1;31m#"
+#				sleep 0.1s
+#			done
+#			[[ -e $HOME/fim ]] && rm $HOME/fim && break
+#			echo -e "\033[1;33m]"
+#			sleep 1s
+#			tput cuu1
+#			tput dl1
+#			echo -ne "\033[1;33mAGUARDE \033[1;37m- \033[1;33m["
+#		done
+#		echo -e "\033[1;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
+#		tput cnorm
+#	}
 
 	verif_ptrs() {
 		porta=$1
