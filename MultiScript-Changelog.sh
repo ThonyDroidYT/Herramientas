9de3c8e9561ce3c21bca68436857abb43765e586
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-01-17 21:48:34 -0500

    Create MultiScript-Changelog.sh

---
+++ MultiScript-Changelog.sh
@@ -0,0 +1,16 @@
+echo -e "\033[1;31mLista de Cambios ${name}${versión}${plain}"
+version="\033[1;35m1.2"
+name="\033[1;31mADM-MANAGER-ALPHA"
+plain="\033[0m"
+cyan="\033[1;36"
+red="\033[1;31m"
+purple="\033[1;35"
+green="\033[1;32m"
+add="\033[1;32mAgregado:"
+fix="\033[1;31mCorregido:"
+cambios () {
+echo -e "${add} SSLH MULTIPLEX ${red}BETA ${plain}
+${fix} Traductor Script Algunas VPS
+"
+}
+cambios
