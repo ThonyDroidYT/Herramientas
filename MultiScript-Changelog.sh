0b7c5a57e103365f33d99650fe80fea4cbca4116
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-01-17 22:01:23 -0500

    Update MultiScript-Changelog.sh

--- MultiScript-Changelog.sh
+++ MultiScript-Changelog.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 echo -e "\033[1;31mLista de Cambios ${name}${versión}${plain}"
 version="\033[1;35m1.2"
-name="\033[1;31mADM-MANAGER-ALPHA"
+name="\033[1;31mMULTISCRIPT-TD"
 plain="\033[0m"
 cyan="\033[1;36"
 red="\033[1;31m"
@@ -8,9 +9,13 @@ purple="\033[1;35"
 green="\033[1;32m"
 add="\033[1;32mAgregado:"
 fix="\033[1;31mCorregido:"
+#CAMBIOS
 cambios () {
-echo -e "${add} SSLH MULTIPLEX ${red}BETA ${plain}
-${fix} Traductor Script Algunas VPS
-"
+echo -e "\033[1;31mLista de Cambios ${name}${versión}${plain}"
+echo -e "${add} ${cyan}SSLH MULTIPLEX ${red}BETA 
+${fix} ${cyan}Traductor Script Algunas VPS
+${add} ${cyan}VPS-PACK ${red}5.8
+${add} ${cyan}ADM-MANAGER-ALPHA-MOD
+${plain}"
 }
 cambios
