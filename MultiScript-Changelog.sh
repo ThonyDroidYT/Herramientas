99fc0bec0198f83de070daef36b06fef31a1a28d
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-01-17 22:20:08 -0500

    Update MultiScript-Changelog.sh

--- MultiScript-Changelog.sh
+++ MultiScript-Changelog.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -e "\033[1;31mLista de Cambios ${name} ${versión} ${plain}"
+#echo -e "\033[1;31mLista de Cambios ${name} ${version} ${plain}"
 version="\033[1;35m1.2"
 name="\033[1;31mMULTISCRIPT-TD"
 plain="\033[0m"
@@ -11,7 +11,7 @@ add="\033[1;32mAgregado:"
 fix="\033[1;31mCorregido:"
 #CAMBIOS
 cambios () {
-echo -e "\033[1;31mLista de Cambios ${name}${versión}${plain}"
+echo -e "\033[1;31mLista de Cambios ${name} ${version} ${plain}"
 echo -e "${add} ${cyan}SSLH MULTIPLEX ${red}BETA 
 ${fix} ${cyan}Traductor Script Algunas VPS
 ${add} ${cyan}VPS-PACK ${red}5.8
