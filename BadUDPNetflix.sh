b217b985a293facec98ff13401a76850a9976966
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-02-12 16:15:56 -0500

    Update BadUDPNetflix.sh

--- BadUDPNetflix.sh
+++ BadUDPNetflix.sh
@@ -12,7 +12,7 @@ touch $HOME/fim
 echo -ne "\033[1;33m ["
 while true; do
    for((i=0; i<18; i++)); do
-   echo -ne "\033[1;31m✦✧"
+   echo -ne "\033[1;31m+-"
    sleep 0.1s
    done
    [[ -e $HOME/fim ]] && rm $HOME/fim && break
@@ -32,11 +32,11 @@ rm -f badvpn-1.999.128.tar.*
 cd ${SCPdir}/
 rm -f badvpn-1.999.128.tar.*
 rm -rf badvpn-1.999.128/
-wget -q https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/badvpn/badvpn-1.999.128.tar.bz2
-tar xf badvpn-1.999.128.tar.bz2
+wget -q https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/badvpn/badvpn-1.999.128.tar.bz2 &>/dev/null
+tar xf badvpn-1.999.128.tar.bz2 &>/dev/null
 cd ${SCPdir}/badvpn-1.999.128
-cmake ${SCPdir}/badvpn-1.999.128 -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_UDPGW=1
-make install
+cmake ${SCPdir}/badvpn-1.999.128 -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_UDPGW=1 &>/dev/null
+make install &>/dev/null
 badvpn-udpgw --listen-addr 127.0.0.1:7200 > /dev/nul &
 echo 'badvpn-udpgw --listen-addr 127.0.0.1:7300 > /dev/nul &' > /etc/rc.local
 #echo 'badvpn-udpgw --listen-addr 127.0.0.1:7200 > /dev/nul &' >> /etc/rc.local
