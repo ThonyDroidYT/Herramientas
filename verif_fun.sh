56c348a3859de7cd3c2dd1203bd91b4e67c40bc9
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-03-08 18:11:04 -0500

    Create verif_fun.sh

--- verif_fun.sh
+++ verif_fun.sh
@@ -1,9 +1,9 @@
-id=$(cat /etc/newadm/idioma)
+#!bin/bash
 [[ ! "$(echo $vo|grep $(cat /usr/bin/vendor_code))" ]] && {
-echo -e "\033[1;31m $(source trans -b pt:${id} "Que Verguenza! Su versión es Pirata")!\033[0m"
+echo -e "\033[1;31m $(fun_trans "Que Verguenza! Su versión es Pirata")!\033[0m"
 echo -e""
-echo -e "\033[1;31m $(source trans -b pt:${id} "Use ADM-PLUS Original")\033[0m"
-echo -e "\033[1;31m $(source trans -b pt:${id} "Ayude a Quien Realmente desarrolla y se Dedica al Projeto!")\033[0m"
+echo -e "\033[1;31m $(fun_trans "Use ADM-PLUS Original")\033[0m"
+echo -e "\033[1;31m $(fun_trans "Ayude a Quien Realmente desarrolla y se Dedica al Projeto")!\033[0m"
 rm -rf ${SCPdir} &>/dev/null
 rm -rf ${SCPusr} &>/dev/null
 rm -rf ${SCPfrm} &>/dev/null
@@ -11,4 +11,8 @@ rm -rf ${SCPinst} &>/dev/null
 rm -rf ${SCPfrm} &>/dev/null
 rm -rf ${SCPidioma} &>/dev/null
 exit 0
+}
+
+#verif 2
+verif_fun2 () {
 }
