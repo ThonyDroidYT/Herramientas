97344d0ccf1a550b65fa7052ba2cc1e1477aa872
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-03-19 07:41:30 -0500

    Update colors.sh

--- colors.sh
+++ colors.sh
@@ -20,7 +20,8 @@ bash <(curl -Ls https://thonydroidyt.github.io/Herramientas/msg.sh) --verm2 "$1"
 }
 fun_trans () {
 texto=$1
-[[ -z /etc/newadm/idioma ]] && id=$(cat /etc/newadm/idioma) || id=es
+#[[ -z /etc/newadm/idioma ]] 
+[[ -e "/etc/newadm/idioma" ]] && id=$(cat /etc/newadm/idioma) || && id=es
 texto2=$(source trans -b es:${id} "$texto")
 #echo -e "$(source trans -b es:${id} "$texto")"
 echo -e "$texto2"
