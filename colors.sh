9e84806dfd6638c566a40c62e683a3a468fdf620
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-03-19 07:57:57 -0500

    Update colors.sh

--- colors.sh
+++ colors.sh
@@ -37,11 +37,11 @@ msg () {
 color () {
 [[ -e "/etc/newadm/idioma" ]] && id2=$(cat /etc/newadm/idioma) || id2=es
 #texto2=$(source trans -b es:${id} "$texto")
-texto2=$(source trans -b es:${id} "${@:2}")
+texto3=$(source trans -b es:${id2} "${@:2}")
 COLOR=$1
 #echo -e "\033[${COLOR}${@:2}\033[0m"
 #echo -e "${COLOR}${@:2}\033[0m"
-#echo -e "${COLOR}${texto2}\033[0m"
+#echo -e "${COLOR}${texto3}\033[0m"
 echo -e "${COLOR}$(source trans -b es:${id2} "${@:2}")\033[0m"
 }
 red="\e[1;31m"
